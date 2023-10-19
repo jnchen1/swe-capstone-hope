fe9d1a892553c1351f7f5a59db148ca92458244a
Tae Hyun Lee <brian528xz@gmail.com>
2023-10-19 12:26:27 -0700

    corrected&completed animation for FindingWhatMattersScoreScreen

--- shared/src/commonMain/kotlin/ui/finding_whatmatters/FindingWhatMattersScoreScreen.kt
+++ shared/src/commonMain/kotlin/ui/finding_whatmatters/FindingWhatMattersScoreScreen.kt
@@ -1,9 +1,22 @@
 package ui.finding_whatmatters
 
 import HomeScreen
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -18,6 +31,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -43,12 +57,17 @@ import androidx.compose.material.icons.rounded.Home
 import androidx.compose.material.icons.rounded.LastPage
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.SpanStyle
@@ -84,12 +103,57 @@ data class FindingWhatMattersScoreScreen(
     @OptIn(ExperimentalResourceApi::class)
     @Composable
     override fun Content() {
+
+        var visible by remember {
+            mutableStateOf(false)
+        }
+
+        val visibleState = remember { MutableTransitionState(false) }
+
+        val animatedAlpha by animateFloatAsState(
+            targetValue = if (visible) 1.0f else 0f,
+            animationSpec = tween(
+                delayMillis = 500,
+                durationMillis = 1000,
+                easing = LinearEasing
+            ),
+            label = "alpha"
+        )
+        val animatedAlpha1 by animateFloatAsState(
+            targetValue = if (visible) 1.0f else 0f,
+            animationSpec = tween(
+                delayMillis = 2500,
+                durationMillis = 1000,
+                easing = LinearEasing
+            ),
+            label = "alpha"
+        )
+        val animatedAlpha2 by animateFloatAsState(
+            targetValue = if (visible) 1.0f else 0f,
+            animationSpec = tween(
+                delayMillis = 4500,
+                durationMillis = 1000,
+                easing = LinearEasing
+            ),
+            label = "alpha"
+        )
+
+
         LifecycleEffect(
-            onStarted = { println("Navigator: Start screen $screenTitle") },
-            onDisposed = { println("Navigator: Dispose screen $screenTitle") }
+            onStarted = {
+                println("Navigator: Start screen $screenTitle")
+                visible = true
+                visibleState.targetState = true
+            },
+            onDisposed = {
+                println("Navigator: Dispose screen $screenTitle")
+            }
         )
 
         val navigator = LocalNavigator.currentOrThrow
+
+
+
 
         Scaffold(
             topBar = {
@@ -119,7 +183,7 @@ data class FindingWhatMattersScoreScreen(
                         )
 
                         Button(
-                            onClick = { navigator.push(FindingWhatMattersScreen())},
+                            onClick = { navigator.push(FindingWhatMattersScreen()) },
                             colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondary),
                             modifier = Modifier.weight(1f),
                             contentPadding = PaddingValues(horizontal = 8.dp, vertical = 2.dp)
@@ -154,7 +218,7 @@ data class FindingWhatMattersScoreScreen(
                         )
 
                         Button(
-                            onClick = { /*TODO : Conclusion Screen */ },
+                            onClick = { /*TODO : Conclusion Screen */},
                             colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondary),
                             modifier = Modifier.weight(1f),
                             contentPadding = PaddingValues(horizontal = 8.dp, vertical = 2.dp)
@@ -194,7 +258,12 @@ data class FindingWhatMattersScoreScreen(
                         modifier = Modifier.fillMaxWidth().padding(8.dp).padding(top = 4.dp),
                         text = buildAnnotatedString {
                             append("The score gives you an idea of where your preference is for the two options.")
-                            withStyle(style = SpanStyle(fontWeight = FontWeight.Bold, fontStyle = FontStyle.Italic)) {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontWeight = FontWeight.Bold,
+                                    fontStyle = FontStyle.Italic
+                                )
+                            ) {
                                 append("\nNote:")
                             }
                             withStyle(style = SpanStyle(fontStyle = FontStyle.Italic)) {
@@ -203,13 +272,7 @@ data class FindingWhatMattersScoreScreen(
                         }
                     )
 
-//                    val lateTextAlpha by animateFloatAsState(
-//                        targetValue = if (shouldShowLateText) 1f else 0f,
-//                        animationSpec = tween(
-//                            durationMillis = 1000,
-//                            easing = LinearEasing
-//                        )
-//                    )
+
 
                     Row(
                         horizontalArrangement = Arrangement.Start,
@@ -220,37 +283,69 @@ data class FindingWhatMattersScoreScreen(
                             modifier = Modifier.padding(it).padding(horizontal = 8.dp).weight(1f)
                         ) {
                             Text(
-                                text="Score of less than 50% indicates increasing preference for usual care",
-                                modifier = Modifier.fillMaxWidth().padding(8.dp).padding(top = 4.dp),
+                                text = "Score of less than 50% indicates increasing preference for usual care",
+                                modifier = Modifier.fillMaxWidth().padding(8.dp)
+                                    .padding(top = 4.dp)
+                                    .graphicsLayer {
+                                        alpha = animatedAlpha1
+                                    },
                                 style = MaterialTheme.typography.caption
                             )
 
-                            Image(
-                                painter = painterResource("finding_whatmatters/arrow_left.png"),
-                                contentDescription = "arrow_left",
-                                contentScale = ContentScale.FillWidth,
-                                modifier = Modifier.fillMaxWidth()
-                                    .align(Alignment.CenterHorizontally)
-                            )
-                        }
-
-                        Column(
-                            modifier = Modifier.padding(it).padding(horizontal = 8.dp).weight(1f)
-                        ) {
-                            Box(
-                                modifier = Modifier.size(50.dp).clip(CircleShape)
-                                    .background(Color(157, 195, 230)),
-                                contentAlignment = Alignment.Center
+                            AnimatedVisibility(
+                                visibleState = visibleState,
+                                enter =  expandHorizontally(
+                                    expandFrom = Alignment.End,
+                                    animationSpec = tween(
+                                        delayMillis = 2000,
+                                        durationMillis = 1000,
+                                        easing = LinearEasing
+                                    )
+                                ),
                             ) {
-                                Text(
-                                    text = "50%",
-                                    color = Color.White,
-                                    textAlign = TextAlign.Center
+                                Image(
+                                    painter = painterResource("finding_whatmatters/arrow_left.png"),
+                                    contentDescription = "arrow_left",
+                                    contentScale = ContentScale.FillWidth,
+                                    modifier = Modifier.fillMaxWidth()
                                 )
                             }
+                        }
+
+
+
+                        Column(
+                            modifier = Modifier.padding(it).weight(1f)
+                                ,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+
+                            AnimatedVisibility(
+                                visibleState = visibleState,
+                                enter = scaleIn(
+                                    animationSpec = tween(
+                                        durationMillis = 500,
+                                        easing = LinearEasing
+                                    )
+                                ),
+                            ) {
+                                Box(
+                                    modifier = Modifier.size(50.dp).clip(CircleShape)
+                                        .background(Color(157, 195, 230)),
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    Text(
+                                        text = "50%",
+                                        color = Color.White,
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            }
                             Text(
-                                text="This indicates you are neutral towards both options.",
-                                modifier = Modifier.fillMaxWidth().padding(8.dp).padding(top = 4.dp),
+                                text = "This indicates you are neutral towards both options.",
+                                modifier = Modifier.fillMaxWidth().padding(top = 4.dp).graphicsLayer {
+                                    alpha = animatedAlpha
+                                },
                                 style = MaterialTheme.typography.caption
                             )
 
@@ -260,70 +355,98 @@ data class FindingWhatMattersScoreScreen(
                             modifier = Modifier.padding(it).padding(horizontal = 8.dp).weight(1f)
                         ) {
                             Text(
-                                text="Score of more than 50% indicates increasing preference for shared care",
-                                modifier = Modifier.fillMaxWidth().padding(8.dp).padding(top = 4.dp),
+                                text = "Score of more than 50% indicates increasing preference for shared care",
+                                modifier = Modifier.fillMaxWidth().padding(8.dp)
+                                    .padding(top = 4.dp).graphicsLayer {
+                                                                       alpha = animatedAlpha2
+                                    },
                                 style = MaterialTheme.typography.caption
                             )
 
-                            Image(
-                                painter = painterResource("finding_whatmatters/arrow_right.png"),
-                                contentDescription = "arrow_right",
-                                contentScale = ContentScale.FillWidth,
-                                modifier = Modifier.fillMaxWidth()
-                                    .align(Alignment.CenterHorizontally)
-                            )
+                            AnimatedVisibility(
+                                visibleState = visibleState,
+                                enter = expandHorizontally(
+                                    animationSpec = tween(
+                                        delayMillis = 4000,
+                                        durationMillis = 1000,
+                                        easing = LinearEasing
+                                    )
+                                ),
+                            ) {
+                                Image(
+                                    painter = painterResource("finding_whatmatters/arrow_right.png"),
+                                    contentDescription = "arrow_right",
+                                    contentScale = ContentScale.FillWidth,
+                                    modifier = Modifier.fillMaxWidth()
+                                )
+                            }
+
                         }
                     }
 
-                    Row(
-                        horizontalArrangement = Arrangement.Start,
-                        modifier = Modifier.fillMaxWidth().padding(bottom = 4.dp)
-                    ) {
-                        Card(
-                            backgroundColor = Color(164,149,146),
-                            shape = RoundedCornerShape(12.dp),
-                            elevation = 0.dp,
-                            modifier = Modifier.fillMaxWidth().weight(1f)
-
+                        Row(
+                            horizontalArrangement = Arrangement.Start,
+                            modifier = Modifier.fillMaxWidth().padding(bottom = 4.dp)
                         ) {
-                            Text("I prefer continuing with usual care.",
-                                color = Color.White,
-                                style = MaterialTheme.typography.body2,
-                                modifier = Modifier.padding(12.dp))
-                        }
+                            Card(
+                                backgroundColor = Color(164, 149, 146),
+                                shape = RoundedCornerShape(12.dp),
+                                elevation = 0.dp,
+                                modifier = Modifier.fillMaxWidth().weight(1f)
 
-                        Card(
-                            modifier = Modifier.fillMaxWidth().weight(2.5f).align(Alignment.CenterVertically),
-                            shape = RectangleShape,
-                            elevation = 0.dp
-                        ) {
-                            Box(
-                                Modifier.background(brush = Brush.horizontalGradient(listOf(Color(164,149,146), Color(233,151,135))))
                             ) {
-                                Text(text= "PREFERENCE SCALE",
+                                Text(
+                                    "I prefer continuing with usual care.",
                                     color = Color.White,
-                                    modifier = Modifier.padding(12.dp).align(Alignment.Center),
-                                    textAlign = TextAlign.Center)
+                                    style = MaterialTheme.typography.body2,
+                                    modifier = Modifier.padding(12.dp)
+                                )
+                            }
+
+                            Card(
+                                modifier = Modifier.fillMaxWidth().weight(2.5f)
+                                    .align(Alignment.CenterVertically),
+                                shape = RectangleShape,
+                                elevation = 0.dp
+                            ) {
+                                Box(
+                                    Modifier.background(
+                                        brush = Brush.horizontalGradient(
+                                            listOf(
+                                                Color(164, 149, 146),
+                                                Color(233, 151, 135)
+                                            )
+                                        )
+                                    )
+                                ) {
+                                    Text(
+                                        text = "PREFERENCE SCALE",
+                                        color = Color.White,
+                                        modifier = Modifier.padding(12.dp).align(Alignment.Center),
+                                        textAlign = TextAlign.Center
+                                    )
+                                }
+                            }
+
+                            Card(
+                                backgroundColor = Color(233, 151, 135),
+                                shape = RoundedCornerShape(12.dp),
+                                elevation = 0.dp,
+                                modifier = Modifier.fillMaxWidth().weight(1f)
+
+                            ) {
+                                Text(
+                                    "I am willing to try out shared care.",
+                                    color = Color.White,
+                                    style = MaterialTheme.typography.body2,
+                                    modifier = Modifier.padding(12.dp)
+                                )
                             }
                         }
 
-                        Card(
-                            backgroundColor = Color(233,151,135),
-                            shape = RoundedCornerShape(12.dp),
-                            elevation = 0.dp,
-                            modifier = Modifier.fillMaxWidth().weight(1f)
-
-                        ) {
-                            Text("I am willing to try out shared care.",
-                                color = Color.White,
-                                style = MaterialTheme.typography.body2,
-                                modifier = Modifier.padding(12.dp))
-                        }
                     }
-
                 }
             }
-        }
 
+        }
     }
-}

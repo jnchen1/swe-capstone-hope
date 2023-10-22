8b25588a0d2753c112a1dce9c0a600084714a573
JiaNi <>
2023-10-22 13:05:28 -0700

    Link to emotional effect screen and cleanup

--- shared/src/commonMain/kotlin/EmotionalEffectSecondScreen.kt
+++ shared/src/commonMain/kotlin/EmotionalEffectSecondScreen.kt
@@ -1,14 +1,14 @@
-
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
@@ -21,17 +21,21 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.ArrowForward
 import androidx.compose.material.icons.rounded.Home
+import androidx.compose.material.icons.rounded.LastPage
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.lifecycle.LifecycleEffect
@@ -42,10 +46,7 @@ import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.sp
 import ui.followup_care.FollowupCareIntroScreen
-
 
 data class EmotionalEffectSecondScreen(
     val wrapContent: Boolean = false
@@ -70,7 +71,7 @@ data class EmotionalEffectSecondScreen(
                     title = {
                         Text(
                             text = screenTitle,
-                            color = Color(0xFFFF7E79),
+                            color = Color(0xFFFFE699),
                             style = MaterialTheme.typography.h1,
                             modifier = Modifier.fillMaxWidth(),
                             textAlign = TextAlign.Center
@@ -113,14 +114,36 @@ data class EmotionalEffectSecondScreen(
                         Button(
                             onClick = { navigator.push(FollowupCareIntroScreen()) },
                             colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondary),
-                            modifier = Modifier.weight(1f).fillMaxHeight()
+                            modifier = Modifier.weight(1f),
+                            contentPadding = PaddingValues(horizontal = 8.dp, vertical = 2.dp)
                         ) {
-                            Text(text = "Next")
+                            BoxWithConstraints {
+                                val boxScope = this
 
-                            Icon(
-                                Icons.Rounded.ArrowForward, "Next page",
-                                Modifier.padding(start = 4.dp)
-                            )
+                                val textSize = MaterialTheme.typography.button.fontSize
+                                val textLayout = rememberTextMeasurer().measure(
+                                    text = "Next section",
+                                    style = MaterialTheme.typography.button,
+                                    overflow = TextOverflow.Clip
+                                )
+                                val sizeInDp = with(LocalDensity.current) {
+                                    textLayout.size.width.toDp()
+                                }
+                                val textAndSize =
+                                    if (boxScope.maxWidth < sizeInDp * 1.3f) Pair(
+                                        "Next\nsection", textSize * .8
+                                    )
+                                    else Pair("Next section", textSize)
+
+                                Text(
+                                    text = textAndSize.first,
+                                    fontSize = textAndSize.second,
+                                    textAlign = TextAlign.End,
+                                    overflow = TextOverflow.Clip,
+                                    modifier = Modifier.padding(end = 4.dp)
+                                )
+                            }
+                            Icon(Icons.Rounded.LastPage, contentDescription = "Next section")
                         }
                     }
                 }
@@ -130,28 +153,23 @@ data class EmotionalEffectSecondScreen(
                 modifier = Modifier.padding(it).padding(horizontal = 8.dp)
                     .verticalScroll(rememberScrollState())
             ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    Image(
-                        painter = painterResource("emotional_effects_4.png"),
-                        contentDescription = "Emotional effects",
-                        contentScale = ContentScale.Fit,
-                        modifier = Modifier.size(200.dp).padding(10.dp)
-                    )
-
-                }
+                Image(
+                    painter = painterResource("emotional_effects_4.png"),
+                    contentDescription = "Emotional effects",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.size(200.dp).padding(8.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
 
                 Text(
-                    style = MaterialTheme.typography.h1,
-                    modifier = Modifier.padding(8.dp),
+                    style = MaterialTheme.typography.h3,
+                    modifier = Modifier.padding(8.dp, 4.dp),
                     text = "How to seek help?"
                 )
 
                 Text(
                     style = MaterialTheme.typography.body1,
-                    modifier = Modifier.padding(8.dp),
+                    modifier = Modifier.padding(horizontal = 8.dp),
                     text = buildAnnotatedString {
                         append("Consider speaking to a ")
                         withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
@@ -163,27 +181,28 @@ data class EmotionalEffectSecondScreen(
                 )
 
                 Text(
-                    style = MaterialTheme.typography.h1,
-                    modifier = Modifier.padding(8.dp),
+                    style = MaterialTheme.typography.h3,
+                    modifier = Modifier.padding(horizontal = 8.dp).padding(top = 12.dp),
                     text = "What can you do?"
                 )
 
                 Text(
                     style = MaterialTheme.typography.body1,
-                    modifier = Modifier.padding(8.dp),
+                    modifier = Modifier.padding(horizontal = 8.dp),
                     text = buildAnnotatedString {
                         append("You may contact the  ")
-                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
-                            append(" National Cancer Centre Singapore")
-                        }
+                        withStyle(SpanStyle(fontWeight = FontWeight.Bold)) { append(" National Cancer Centre Singapore") }
                         append(" for their services through the following: ")
                     }
                 )
 
-                PhoneLinkText()
+                PhoneLinkText(
+                    Modifier.padding(horizontal = 8.dp).padding(top = 8.dp).fillMaxWidth()
+                )
 
-                EmailLinkText()
-
+                EmailLinkText(
+                    Modifier.padding(8.dp).fillMaxWidth()
+                )
             }
         }
     }
@@ -191,154 +210,81 @@ data class EmotionalEffectSecondScreen(
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-fun PhoneLinkText() {
+fun PhoneLinkText(modifier: Modifier = Modifier) {
     val phoneNumber = "+65 6558 0384"
-    val phoneNumber2 = "\n+65 6558 0384"
 
     val uriHandler = LocalUriHandler.current
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-
+    Row(verticalAlignment = Alignment.Top, modifier = modifier) {
         // Add the image here
         Image(
             painter = painterResource("emotional_effects_5.png"), // Replace 'your_image' with the actual image resource
             contentDescription = null, // Provide a content description as needed
-            modifier = Modifier.size(24.dp) // Adjust the size as needed
+            modifier = Modifier.padding(end = 4.dp).size(24.dp)
+                .fillMaxWidth(.1f) // Adjust the size as needed
         )
 
         // Add the text
-        Text(
-            style = MaterialTheme.typography.body1.copy(fontSize = 13.sp),
-            text = buildAnnotatedString {
-                append("Psychological Services, Medical Social Services, Counselling ")
-            }
-        )
-
-
-    }
-
-
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-
-        Text(
-            style = MaterialTheme.typography.body1.copy(fontSize = 13.sp),
-            text = buildAnnotatedString {
-                append("and Financial Assistance: ")
-            }
-        )
-
-        Text(
-            text = buildAnnotatedString {
-                withStyle(style = SpanStyle(color = Color.Blue, fontWeight = FontWeight.Bold, fontSize = 13.sp)) {
+        Column {
+            val serviceText = buildAnnotatedString {
+                append("Psychological Services, Medical Social Services, Counselling and Financial Assistance: ")
+                withStyle(SpanStyle(Color.Blue, fontWeight = FontWeight.Bold)) {
+                    pushStringAnnotation(tag = phoneNumber, annotation = phoneNumber)
                     append(phoneNumber)
                 }
-
-            },
-            modifier = Modifier.clickable {
-                uriHandler.openUri("tel:$phoneNumber")
             }
-        )
+            ClickableText(text = serviceText, onClick = { offset ->
+                serviceText.getStringAnnotations(offset, offset)
+                    .firstOrNull()?.let { uriHandler.openUri("tel:$phoneNumber") }
+            })
 
-    }
-
-
-
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Text(
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.padding(0.dp),
-            text = buildAnnotatedString {
-                withStyle(style = SpanStyle(fontSize = 13.sp)) {
-                    append("\nPatient Support Programmes: ")
+            val programText = buildAnnotatedString {
+                append("Patient Support Programmes: ")
+                withStyle(SpanStyle(Color.Blue, fontWeight = FontWeight.Bold)) {
+                    pushStringAnnotation(tag = phoneNumber, annotation = phoneNumber)
+                    append(phoneNumber)
                 }
-
             }
-        )
-
-        Text(
-            text = buildAnnotatedString {
-                withStyle(style = SpanStyle(color = Color.Blue, fontWeight = FontWeight.Bold, fontSize = 13.sp)) {
-                    append(phoneNumber2)
-                }
-
-            },
-            modifier = Modifier.clickable {
-                uriHandler.openUri("tel:$phoneNumber2")
-            }
-        )
-
-
-
+            ClickableText(text = programText, Modifier.padding(top = 4.dp), onClick = { offset ->
+                programText.getStringAnnotations(offset, offset)
+                    .firstOrNull()?.let { uriHandler.openUri("tel:$phoneNumber") }
+            })
+        }
     }
-
-
-
 }
-
-
-
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-fun EmailLinkText() {
+fun EmailLinkText(modifier: Modifier = Modifier) {
     val emailAddress = "psychosocial@nccs.com.sg"
 
     val uriHandler = LocalUriHandler.current
 
     Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
+        verticalAlignment = Alignment.Top,
+        modifier = modifier
     ) {
         // Add the image here
         Image(
             painter = painterResource("emotional_effects_6.png"), // Replace 'your_image' with the actual image resource
             contentDescription = null, // Provide a content description as needed
-            modifier = Modifier.size(24.dp) // Adjust the size as needed
+            modifier = Modifier.padding(end = 4.dp).size(24.dp) // Adjust the size as needed
         )
 
         // Add the text
-        Text(
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.padding(0.dp),
-            text = buildAnnotatedString {
-                withStyle(style = SpanStyle(fontSize = 13.sp)) {
-                    append("Email at ")
-                }
+        val emailText = buildAnnotatedString {
+            append("Email at ")
+            withStyle(SpanStyle(Color.Blue, fontWeight = FontWeight.Bold)) {
+                pushStringAnnotation(tag = emailAddress, annotation = emailAddress)
+                append(emailAddress)
             }
-        )
-
-        Text(
-            text = buildAnnotatedString {
-                withStyle(style = SpanStyle(color = Color.Blue, fontWeight = FontWeight.Bold, fontSize = 13.sp)) {
-                    append(emailAddress)
-                }
-
-            },
-            modifier = Modifier.clickable {
-                uriHandler.openUri("mailto:$emailAddress")
-            }
-        )
-
-        Text(
-            style = MaterialTheme.typography.body1,
-            text = buildAnnotatedString {
-                withStyle(style = SpanStyle(fontSize = 13.sp)) {
-                    append(" for more details")
-                }
-            }
-        )
-
+            append(" for more details")
+        }
+        ClickableText(text = emailText, Modifier.padding(top = 4.dp), onClick = { offset ->
+            emailText.getStringAnnotations(offset, offset)
+                .firstOrNull()?.let { uriHandler.openUri("mailto:$emailAddress") }
+        })
     }
-
 }
 
 

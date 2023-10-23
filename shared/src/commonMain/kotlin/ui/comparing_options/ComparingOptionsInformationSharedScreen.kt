26df99b7882d8681fb6430d70c21bfd375f0e303
Sam-Salerno <samuelsalerno@yahoo.com>
2023-10-23 15:48:06 -0700

    Add first two screen of Comparing options slides

---
+++ shared/src/commonMain/kotlin/ui/comparing_options/ComparingOptionsInformationSharedScreen.kt
@@ -0,0 +1,289 @@
+package ui.comparing_options
+
+import HomeScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowForward
+import androidx.compose.material.icons.rounded.FirstPage
+import androidx.compose.material.icons.rounded.Home
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.lifecycle.LifecycleEffect
+import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.core.screen.ScreenKey
+import cafe.adriel.voyager.core.screen.uniqueScreenKey
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import ui.followup_care.FollowupCareOptionScreen
+
+data class ComparingOptionsInformationSharedScreen(
+    val wrapContent: Boolean = false
+) : Screen {
+
+    override val key: ScreenKey = uniqueScreenKey
+    private val screenTitle = "COMPARING THE OPTIONS"
+
+    @OptIn(ExperimentalResourceApi::class)
+    @Composable
+    override fun Content() {
+        LifecycleEffect(
+            onStarted = { println("Navigator: Start screen $screenTitle") },
+            onDisposed = { println("Navigator: Dispose screen $screenTitle") }
+        )
+
+        val navigator = LocalNavigator.currentOrThrow
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(
+                            text = screenTitle,
+                            style = MaterialTheme.typography.h1,
+                            color = Color(0xFFC61E1DC),
+                            modifier = Modifier.fillMaxWidth(),
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                )
+            },
+            bottomBar = {
+                BottomNavigation {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(horizontal = 12.dp, vertical = 8.dp).padding(bottom = 4.dp)
+                    ) {
+                        val textSize = MaterialTheme.typography.button.fontSize
+                        val textLayout = rememberTextMeasurer().measure(
+                            text = "Previous section",
+                            style = MaterialTheme.typography.button,
+                            overflow = TextOverflow.Clip
+                        )
+
+                        Button(
+                            onClick = {
+//                                if (navigator.items.contains(FollowupCareScreen())) {
+//                                    navigator.pop()
+//                                } else {
+//                                    navigator.replace(FollowupCareScreen())
+//                                }
+                            },
+                            colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondary),
+                            modifier = Modifier.weight(1f),
+                            contentPadding = PaddingValues(horizontal = 8.dp, vertical = 2.dp)
+                        ) {
+                            Icon(
+                                Icons.Rounded.FirstPage,
+                                contentDescription = "Previous section"
+                            )
+                            BoxWithConstraints {
+                                val boxScope = this
+                                val sizeInDp = with(LocalDensity.current) {
+                                    textLayout.size.width.toDp()
+                                }
+
+                                Text(
+                                    text = "Previous section",
+                                    fontSize = if (boxScope.maxWidth - 4.dp < sizeInDp) textSize * .8 else textSize,
+                                    overflow = TextOverflow.Clip,
+                                    modifier = Modifier.padding(start = 8.dp)
+                                )
+                            }
+                        }
+
+                        BottomNavigationItem(
+                            selected = false,
+                            onClick = { navigator.popUntil { it == HomeScreen() } },
+                            icon = { Icon(Icons.Rounded.Home, "Home", tint = Color.White) },
+                            label = { Text(text = "Home", color = Color.White) },
+                            modifier = Modifier.weight(1f)
+                        )
+
+                        Button(
+                            onClick = { navigator.push(FollowupCareOptionScreen()) },
+                            colors = ButtonDefaults.buttonColors(MaterialTheme.colors.secondary),
+                            modifier = Modifier.weight(1f).fillMaxHeight()
+                        ) {
+                            Text(text = "Next")
+
+                            Icon(
+                                Icons.Rounded.ArrowForward, "Next page",
+                                Modifier.padding(start = 4.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        ) {
+            Column(
+                modifier = Modifier.padding(it).padding(horizontal = 8.dp)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Card(
+                    elevation = 2.dp,
+                    modifier = Modifier.padding(10.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    backgroundColor = Color.White
+                ){
+                    Column(modifier = Modifier.padding(4.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally){
+                        Box(
+                            modifier = Modifier.fillMaxWidth(0.5f).clip(RoundedCornerShape(20.dp))
+                                .background(Color(0xFFA49592))
+                        ){
+                            Text(text = "Usual Care",
+                                style = MaterialTheme.typography.h2,
+                                color = Color.White,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+
+                        Image(
+                            painter = painterResource("comparing_options/comparing_usual_information_shared.png"),
+                            contentDescription = "How usual care share information",
+                            modifier = Modifier.width(323.dp).height(278.dp).padding(8.dp,top= 10.dp),
+                            contentScale = ContentScale.FillBounds
+                        )
+                        Text(
+                            style = MaterialTheme.typography.body1,
+                            modifier = Modifier.padding(8.dp).align(Alignment.CenterHorizontally),
+                            text = buildAnnotatedString {
+                                append("Health care professionals caring for you ")
+                                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                    append("may or may")
+                                }
+                                append(" know about your cancer and treatment")
+                            }
+                        )
+
+                    }
+                }
+
+                Button(
+                    enabled = true,
+                onClick = {
+                    if (navigator.items.contains(ComparingOptionsIntroScreen())) {
+                        navigator.pop()
+                    } else {
+                        navigator.replace(ComparingOptionsIntroScreen())
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(Color(0xFF727077)),
+                ){
+                    Text("Who is caring for you?")
+                }
+                Button(
+                    enabled = false,
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(Color(0xFF727077)),
+                ){
+                    Text("How is information about you shared?")
+                }
+                Button(
+                    enabled = true,
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(Color(0xFF727077)),
+                ){
+                    Text("What is your follow-up schedule?")
+                }
+                Button(
+                    enabled = true,
+                    onClick = {},
+                    colors = ButtonDefaults.buttonColors(Color(0xFF727077)),
+                ){
+                    Text("Cost?")
+                }
+
+                Card(
+                    elevation = 2.dp,
+                    modifier = Modifier.padding(10.dp),
+                    shape = RoundedCornerShape(20.dp),
+                    backgroundColor = Color.White
+                ){
+                    Column(modifier = Modifier.padding(4.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally){
+                        Box(
+                            modifier = Modifier.fillMaxWidth(0.5f).clip(RoundedCornerShape(20.dp))
+                                .background(Color(0xFFE99787))
+                        ){
+                            Text(text = "Shared Care",
+                                style = MaterialTheme.typography.h2,
+                                color = Color.White,
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+
+                        Image(
+                            painter = painterResource("comparing_options/comparing_shared_information_shared.png"),
+                            contentDescription = "How shared care share information",
+                            modifier = Modifier.width(323.dp).height(278.dp).padding(8.dp,top= 10.dp),
+                            contentScale = ContentScale.FillBounds
+
+                        )
+                        Text(
+                            style = MaterialTheme.typography.body1,
+                            modifier = Modifier.padding(8.dp).align(Alignment.CenterHorizontally),
+                            text = buildAnnotatedString {
+                                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                    append("All")
+                                }
+                                append(" health care professionals caring for you have a ")
+                                withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                                    append("basic understanding")
+                                }
+                                append(" of your cancer and treatment")
+                            }
+                        )
+
+
+
+                    }
+                }
+
+            }
+        }
+    }
+}

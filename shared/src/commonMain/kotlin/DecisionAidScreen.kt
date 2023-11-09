527af75867f8ccccabfb0dac722302c45c4b6633
Tae Hyun Lee <brian528xz@gmail.com>
2023-11-09 00:08:38 -0800

    created and finished animation for navigation guide

--- shared/src/commonMain/kotlin/DecisionAidScreen.kt
+++ shared/src/commonMain/kotlin/DecisionAidScreen.kt
@@ -1,5 +1,17 @@
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -10,8 +22,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -36,20 +53,53 @@ data class DecisionAidScreen(
     override val key: ScreenKey = uniqueScreenKey
     private val screenTitle = "DECISION AID"
 
-    @OptIn(ExperimentalResourceApi::class)
+    @OptIn(ExperimentalResourceApi::class, ExperimentalAnimationApi::class)
     @Composable
     override fun Content() {
+
+        val visibleState = remember {
+            MutableTransitionState(false)}
+
+        val visibleState2 = remember {
+            MutableTransitionState(false)}
+
+        val visibleState3 = remember {
+            MutableTransitionState(false)}
+
+        val visibleState4 = remember {
+            MutableTransitionState(false)}
+
+        var visible by remember {
+            mutableStateOf(false)
+        }
+
+        val animatedAlpha by animateFloatAsState(
+            targetValue = if (visible) 1.0f else 0f,
+            animationSpec = tween(
+                delayMillis = 1000,
+                durationMillis = 1000,
+                easing = LinearEasing
+            ),
+            label = "alpha"
+        )
+
+
         LifecycleEffect(
-            onStarted = { println("Navigator: Start screen $screenTitle") },
-            onDisposed = { println("Navigator: Dispose screen $screenTitle") }
+            onStarted = {
+                println("Navigator: Start screen $screenTitle") },
+            onDisposed = {
+                println("Navigator: Dispose screen $screenTitle") }
         )
 
         val navigator = LocalNavigator.currentOrThrow
+
+
 
         Scaffold(
             topBar = { ThemeTopAppBar(screenTitle) },
             bottomBar = {
                 ThemeBottomNavigation(
+                    showPrevPage = true, prevAction = { navigator.push(DisclaimerScreen()) },
                     showNextPage = true, nextAction = { navigator.push(WhoIsThisForScreen()) }
                 )
             }
@@ -100,15 +150,128 @@ data class DecisionAidScreen(
                 )
 
                 Button(
-                    onClick = {},
+                    onClick = {
+                        visibleState4.targetState = false
+                        visibleState.targetState = true
+                              },
                     colors = ButtonDefaults.buttonColors(
                         MaterialTheme.colors.secondary,
                         disabledBackgroundColor = MaterialTheme.colors.secondary
                     ),
-                    modifier = Modifier.padding(horizontal = 8.dp),
-                    enabled = false
+                    modifier = Modifier.padding(horizontal = 8.dp)
                 ) {
                     Text("Click here for navigation guide")
+                }
+
+                AnimatedVisibility(
+                    visibleState = visibleState,
+                    enter = fadeIn(
+                        animationSpec = tween(
+                            delayMillis = 1000,
+                            durationMillis = 1000,
+                            easing = LinearEasing
+                        )
+                    ),
+                    exit = fadeOut(
+                        animationSpec = tween(
+                            delayMillis = 1000,
+                            durationMillis = 1000,
+                            easing = LinearEasing
+                        ),
+                    )
+                ) {
+                    Image(
+                        painter = painterResource("guide_nav_bar.png"),
+                        contentDescription = "guide_nav_bar",
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier.fillMaxWidth(0.6f)
+                    )
+
+                    if(this.transition.currentState == this.transition.targetState) {
+                        visibleState.targetState = false
+                        visibleState2.targetState = true
+                    }
+                }
+
+
+                AnimatedVisibility(
+                    visibleState = visibleState2,
+                    enter = fadeIn(
+                        animationSpec = tween(
+                            delayMillis = 3000,
+                            durationMillis = 1000,
+                            easing = LinearEasing
+                        )
+                    ),
+                    exit = fadeOut(
+                        animationSpec = tween(
+                            delayMillis = 1000,
+                            durationMillis = 1000,
+                            easing = LinearEasing
+                        ),
+                    )
+                ) {
+                    Image(
+                        painter = painterResource("guide_back.png"),
+                        contentDescription = "guide_back",
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier.fillMaxWidth(0.4f)
+                    )
+
+                    if(this.transition.currentState == this.transition.targetState) {
+                        visibleState2.targetState = false
+                        visibleState3.targetState = true
+                    }
+                }
+
+                AnimatedVisibility(
+                    visibleState = visibleState3,
+                    modifier = Modifier.align(Alignment.End),
+                    enter = fadeIn(
+                        animationSpec = tween(
+                            delayMillis = 3000,
+                            durationMillis = 1000,
+                            easing = LinearEasing
+                        )
+                    ),
+                    exit = fadeOut(
+                        animationSpec = tween(
+                            delayMillis = 1000,
+                            durationMillis = 1000,
+                            easing = LinearEasing
+                        ),
+                    )
+                ) {
+                    Image(
+                        painter = painterResource("guide_next.png"),
+                        contentDescription = "guide_next",
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier.fillMaxWidth(0.4f).align(Alignment.End)
+                    )
+
+                    if(this.transition.currentState == this.transition.targetState) {
+                        visibleState3.targetState = false
+                        visibleState4.targetState = true
+                    }
+                }
+
+                AnimatedVisibility(
+                    visibleState = visibleState4,
+                    modifier = Modifier.align(Alignment.End),
+                    enter = fadeIn(
+                        animationSpec = tween(
+                            delayMillis = 3000,
+                            durationMillis = 1000,
+                            easing = LinearEasing
+                        )
+                    ),
+                ) {
+                    Image(
+                        painter = painterResource("guide_continue.png"),
+                        contentDescription = "guide_next",
+                        contentScale = ContentScale.FillWidth,
+                        modifier = Modifier.fillMaxWidth(0.5f).align(Alignment.End)
+                    )
                 }
             }
         }
